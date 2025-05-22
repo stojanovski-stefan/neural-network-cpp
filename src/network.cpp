c04043df2c69e67a38e12c76c270760f4147c2b0
Stefan <ss.stojanovski1@gmail.com>
2025-05-22 10:27:11 -0400

    implement neural network for 2 input neurons

---
+++ src/network.cpp
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2025 Stefan Stojanovski
+ */
+
+#include <iostream>
+#include<cmath>
+#include<algorithm>
+#include<vector>
+#include "../include/neuron.h"
+#include "../include/network.h"
+
+using std::vector;
+
+
+Network::Network() {
+    this->weights = {0, 1};
+    this->bias = 0;
+    this->h1 = Neuron(this->weights, this->bias);
+    this->h2 = Neuron(this->weights, this->bias);
+    this->o1 = Neuron(this->weights, this->bias);
+}
+
+float Network::feedForward(vector<float> inputs) {
+    float outH1 = this->h1.feedForward(inputs);
+    float outH2 = this->h2.feedForward(inputs);
+    float outO1 = this->o1.feedForward({outH1, outH2});
+    return outO1;
+}
+
+int main() {
+    vector<float> inputs = {2, 3};
+    Network net = Network();
+    std::cout << net.feedForward(inputs) << std::endl;
+    return 0;
+}
